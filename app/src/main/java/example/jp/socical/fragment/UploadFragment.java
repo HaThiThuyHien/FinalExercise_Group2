8fa107e3fcbae087cca318a4e38f8b1fa4a576d5
HaThiThuyHien <thuyhienctu@gmail.com>
2016-10-19 14:11:08 +0700

    update layout uploadframnet

--- app/src/main/java/example/jp/socical/fragment/UploadFragment.java
+++ app/src/main/java/example/jp/socical/fragment/UploadFragment.java
@@ -1,12 +1,6 @@
 package example.jp.socical.fragment;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import example.jp.socical.R;
 
